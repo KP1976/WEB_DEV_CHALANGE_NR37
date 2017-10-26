f8806b78ada3958a8066be17ef64fd378377fb4a
Krzysztof Pawłowski <krzyszp@wp.pl>
2017-10-26 18:12:09 +0200

    Add files via upload

--- js/search.js
+++ js/search.js
@@ -9,7 +9,7 @@ let Search = (function () {
 
   function search() {
     s.searchIcon.style.opacity = (s.searchIcon.style.opacity === '0.2') ? '1' : '0.2';
-    s.searchInput.style.width = (s.searchInput.style.width === '20%') ? '0' : '20%';
+    s.searchInput.className = (s.searchInput.className === "search-input searchOn") ? "search-input" : "search-input searchOn";  
     s.searchInput.style.padding = (s.searchInput.style.padding === '5px') ? '0' : '5px';
   }
 
